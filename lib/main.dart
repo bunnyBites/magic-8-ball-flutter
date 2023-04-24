c4727920069302b1861e2ca51d2b6fe6cba90e63
bunnyBites <s.mukeshkr481@gmail.com>
2023-04-24 22:14:52 +0530

    1. add style changes 2. remove highlight color for TextButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,52 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
+      const MaterialApp(
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple.shade400,
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple.shade800,
+        title: const Text("Wish For An Answer"),
+      ),
+      body: const Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        style: ButtonStyle(
+          overlayColor: MaterialStateProperty.all(Colors.transparent),
+        ),
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(4) + 1;
+          });
+        },
+        child: Image.asset("images/ball$ballNumber.png"),
+      ),
+    );
+  }
+}
